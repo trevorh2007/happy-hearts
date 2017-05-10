eb9866bd585f27cf4537c5711ba80188f06f0719
Trevor <trevorhoward2@hotmail.com>
2017-05-09 18:14:16 -0600

    Implemented navbar for home page, added javascript files, imported materialize css and fonts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'gallery', to: 'pages#gallery'
+
+root to: 'pages#home'
+
 end
