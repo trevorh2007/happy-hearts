61e8afa5b12ab889f599f440112e699647164f45
Trevor <trevorhoward2@hotmail.com>
2017-05-13 19:42:16 -0600

    styles for about page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	def login_helper style = ''
 	  (link_to "Login", new_admin_session_path, class: style) +
-	  " ".html_safe +
+	  " / ".html_safe +
 	  (link_to "Logout", destroy_admin_session_path, :method => :delete, class: style)
   end
 
